62fa44b1373cd19e7c1e7bfb9b3a09dc707ff699
Farhan Syed <nearhan@gmail.com>
2013-11-03 18:57:12 -0500

    removed pep 8 issues and cache files

--- chatapp/cmdline.py
+++ chatapp/cmdline.py
@@ -1,8 +1,7 @@
 import cmd
-import sys
+import ipdb
 import zmq
 from config import config
-from multiprocessing import Process
 from zmq.devices import ProcessDevice
 
 
@@ -20,35 +19,15 @@ class CLI(cmd.Cmd):
         self.client_socket.send_string(line)
         msg = self.client_socket.recv_string()
         print msg
-        pass
 
     def do_receive(self, line):
         """
         Should be able to remove this.
         """
         self.client_socket.recv_string()
-        pass
 
     def do_debug(self, line):
-        import pdb
-        pdb.set_trace()
-
-
-#class ClientSocketPoller(Process):
-#
-#    def run(self):
-#        context = zmq.Context()
-#        polled_socket = context.socket(zmq.REQ)
-#        polled_socket.connect("tcp://%s:%s" % (config['client_ip'], config['client_port']))
-#        poller = zmq.Poller()
-#        poller.register(polled_socket, zmq.POLLIN)
-#
-#        continue_status = True
-#        while continue_status:
-#            socks = dict(poller.poll())
-#            if polled_socket in socks and socks['polled_socket'] == zmq.POLLIN:
-#                msg = polled_socket.recv()
-#                print msg
+        ipdb.set_trace()
 
 if __name__ == '__main__':
 
@@ -56,7 +35,5 @@ if __name__ == '__main__':
     queue_process.connect_out("tcp://%s:%s" % (config['server_ip'], config['server_port']))
     queue_process.bind_in("tcp://%s:%s" % (config['client_ip'], config['client_port']))
     queue_process.start()
-    #poll_process = ClientSocketPoller()
-    #poll_process.start()
     CLI().cmdloop(intro='Welcome to the shell, mortal.')
 
